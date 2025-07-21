5a55f54bc188933dbd62eba59e1fc5e67abcaa04
sdsouto <sdsouto3@gmail.com>
2025-07-21 14:02:29 -0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,52 +1,35 @@
 /*
-=============================================================
-Create Database and Schemas
-=============================================================
-Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
-	
+**************************************************************************************************
+Script:		
+	=============================================================
+	Create Database and Schemas
+	=============================================================
+
+Description:        
+	This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+	If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+	within the database: 'bronze', 'silver', and 'gold'.
+
+Create Date:
+	2025-07-21
+
+Used By:
+	SQL Data Warehouse Project
+
+Usage:              
+
 WARNING:
     Running this script will drop the entire 'DataWarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
-*/
-/***************************************************************************************************
-Procedure:          dbo.usp_DoSomeStuff
-Create Date:        2018-01-25
-Author:             Joe Expert
-Description:        Verbose description of what the query does goes here. Be specific and don't be
-                    afraid to say too much. More is better, than less, every single time. Think about
-                    "what, when, where, how and why" when authoring a description.
-Call by:            [schema.usp_ProcThatCallsThis]
-                    [Application Name]
-                    [Job]
-                    [PLC/Interface]
-Affected table(s):  [schema.TableModifiedByProc1]
-                    [schema.TableModifiedByProc2]
-Used By:            Functional Area this is use in, for example, Payroll, Accounting, Finance
-Parameter(s):       @param1 - description and usage
-                    @param2 - description and usage
-Usage:              EXEC dbo.usp_DoSomeStuff
-                        @param1 = 1,
-                        @param2 = 3,
-                        @param3 = 2
-                    Additional notes or caveats about this object, like where is can and cannot be run, or
-                    gotchas to watch for when using it.
-****************************************************************************************************
-SUMMARY OF CHANGES
-Date(yyyy-mm-dd)    Author              Comments
-------------------- ------------------- ------------------------------------------------------------
-2012-04-27          John Usdaworkhur    Move Z <-> X was done in a single step. Warehouse does not
-                                        allow this. Converted to two step process.
-                                        Z <-> 7 <-> X
-                                            1) move class Z to class 7
-                                            2) move class 7 to class X
 
-2018-03-22          Maan Widaplan       General formatting and added header information.
-2018-03-22          Maan Widaplan       Added logic to automatically Move G <-> H after 12 months.
-***************************************************************************************************/
+History
+Date(yyyy-mmdd)		Author				Comments
+------------------- ------------------- ----------------------------------------------------------
+2025-0721			Sharon Souto		Initial Version
+
+**************************************************************************************************
+*/
 
 USE master;
 GO
