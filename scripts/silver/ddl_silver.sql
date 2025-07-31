af564b670b067a7d34304ed26c1eec75add61c57
sdsouto <sdsouto3@gmail.com>
2025-07-31 11:55:40 -0400

    Update ddl_silver.sql
    2025-0731           Sharon Souto        Updated some column names, datatypes based on load scripts

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -16,6 +16,7 @@ History:
 Date(yyyy-mmdd)		Author				Comments
 ------------------- ------------------- ----------------------------------------------------------
 2025-0722			Sharon Souto		Initial Version
+2025-0731           Sharon Souto        Updated some column names, datatypes based on load scripts
 
 **************************************************************************************************
 */
@@ -45,12 +46,13 @@ GO
 
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
+    cat_id       NVARCHAR(50),
     prd_key      NVARCHAR(50),
     prd_nm       NVARCHAR(50),
     prd_cost     INT,
     prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME,
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
     dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
