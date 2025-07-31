ab4b4559bbfa0e361b191f2671f6a9555255c08d
sdsouto <sdsouto3@gmail.com>
2025-07-31 12:52:43 -0400

    Update Silver scripts
    Update silver scripts to include crm_cust_info

--- scripts/silver/silver_checks.sql
+++ scripts/silver/silver_checks.sql
@@ -42,3 +42,120 @@ where cst_key in(
 
 select * from silver.crm_cust_info
 
+/*=================================================
+crm_prd_info
+=================================================*/
+--select * from bronze.crm_prd_info
+SELECT 
+prd_id,
+REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id,
+REPLACE(SUBSTRING(prd_key, 7, len(prd_key)),'-','_') AS prd_key,
+prd_nm,
+ISNULL(prd_cost, 0) AS prd_cost,
+CASE UPPER(TRIM(prd_line))
+	WHEN 'M' THEN 'Mountain'
+	WHEN 'R' THEN 'Road'
+	WHEN 'S' THEN 'Other Sales'
+	WHEN 'T' THEN 'Touring'
+	ELSE 'n/a' END AS prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt,
+CAST(LEAD(CAST(prd_start_dt AS DATETIME)) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info
+
+
+-- Check for Nulls or Duplicates in Primary Key
+-- Expectation: No Results
+select prd_id, count(*)
+from silver.crm_prd_info
+group by prd_id
+having count(*) > 1 or prd_id is null
+
+--derive new key from crm_prd_info.prd_key to match erp_px_cat_g1v2.id
+select prd_key
+from silver.crm_prd_info
+select id
+from silver.erp_px_cat_g1v2
+
+SELECT 
+REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id
+FROM silver.crm_prd_info
+
+--check the join
+-- the transformation works, and there happens to be 7 rows not matching
+select * from
+(
+SELECT 
+REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id
+FROM bronze.crm_prd_info) as c join bronze.erp_px_cat_g1v2 e
+on c.cat_id = e.id
+
+select * from bronze.crm_prd_info
+where REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') 
+not in (select id from  bronze.erp_px_cat_g1v2)
+
+
+
+--derive new key from crm_prd_info.prd_key to match crm_sales_details.sls_prd_key
+select prd_key
+from bronze.crm_prd_info
+select sls_prd_key
+from bronze.crm_sales_details
+
+SELECT 
+REPLACE(SUBSTRING(prd_key, 7, len(prd_key)),'-','_') AS prd_key
+FROM bronze.crm_prd_info
+
+--There are many that do not match.  The key looks correct.  Therefore, this means that there are products with no orders.
+select * from  bronze.crm_prd_info
+where REPLACE(SUBSTRING(prd_key, 7, len(prd_key)),'-','_') not in (select sls_prd_key
+from bronze.crm_sales_details)
+
+-- Check for unwanted spaces in string values.
+-- Expection: No Results
+SELECT
+prd_nm
+FROM silver.crm_prd_info
+WHERE TRIM(prd_nm) <> prd_nm OR prd_nm IS NULL
+
+--Check for NULLs or Negative Numbers
+--Expectation: No Results
+SELECT
+prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL
+
+SELECT
+DISTINCT(prd_line),
+CASE 
+	WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+	WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+	WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'other sales'
+	WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+	ELSE 'n/a' END AS prd_line
+FROM bronze.crm_prd_info
+
+SELECT DISTINCT(prd_line) FROM silver.crm_prd_info
+
+--Check for Invalid Dates
+--There are invalid dates in end date
+SELECT
+DISTINCT(ISDATE(prd_start_dt)), prd_start_dt
+--,
+--DISTINCT(ISDATE(prd_end_dt)), prd_end_dt
+FROM bronze.crm_prd_info
+
+--Check for Incorrect Date Ordering
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt
+
+SELECT
+prd_key,
+prd_start_dt,
+prd_end_dt,
+LEAD(CAST(prd_start_dt AS DATETIME)) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS prd_end_dt_TEST
+FROM bronze.crm_prd_info
+WHERE prd_key IN ('AC-HE-HL-U509-R','AC-HE-HL-U509')
+ORDER BY prd_id
+
+SELECT * FROM silver.crm_prd_info
--- scripts/silver/silver_transformations.sql
+++ scripts/silver/silver_transformations.sql
@@ -1,3 +1,6 @@
+USE [DataWarehouse]
+GO
+
 --TRUNCATE TABLE silver.crm_cust_info
 
 INSERT INTO silver.crm_cust_info (
@@ -33,5 +36,38 @@ INSERT INTO silver.crm_cust_info (
             WHERE cst_id IS NOT NULL
             ) AS t
         WHERE flag_last = 1 -- Remove duplicates to ensure only one record per entity by identifying and retaining the most relevant row
+
+
+--TRUNCATE TABLE silver.crm_prd_info
+INSERT INTO [silver].[crm_prd_info]
+    ([prd_id]
+    ,[cat_id]
+    ,[prd_key]
+    ,[prd_nm]
+    ,[prd_cost]
+    ,[prd_line]
+    ,[prd_start_dt]
+    ,[prd_end_dt])
+SELECT 
+    prd_id,
+    REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id, --Derive category id
+    REPLACE(SUBSTRING(prd_key, 7, len(prd_key)),'-','_') AS prd_key, --Derive product key
+    prd_nm,
+    ISNULL(prd_cost, 0) AS prd_cost,
+    CASE UPPER(TRIM(prd_line))
+	    WHEN 'M' THEN 'Mountain'
+	    WHEN 'R' THEN 'Road'
+	    WHEN 'S' THEN 'Other Sales'
+	    WHEN 'T' THEN 'Touring'
+	    ELSE 'n/a' --Handling missing data by filling in blanks by adding a default value
+    END AS prd_line, --Data Normalization & Standardization to map product line codes to descriptive values
+    CAST(prd_start_dt AS DATE) AS prd_start_dt, --Data Transformation: Remove time from date
+    CAST(
+        LEAD(CAST(prd_start_dt AS DATETIME)) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1 
+        AS DATE
+    ) AS prd_end_dt -- Data Enrichment: Calculate end date as one day before the next start date and present without time
+FROM bronze.crm_prd_info
+GO
+
 
 
